632b830a48fd480399b5b29909f15e45a5c55b58
Spaghetti Jones <connor.breault@comcast.net>
2021-10-12 21:03:49 -0700

    initial commit, done after completion of course

---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+import AccountNavigator from "./AccountNavigtor";
+import expoPushTokensApi from "../api/expoPushTokens";
+import FeedNavigator from "./FeedNavigator";
+import ListingEditScreen from "../screens/ListingEditScreen";
+import NewListingButton from "./NewListingButton";
+import navigation from "./rootNavigation";
+import useNotifications from "../hooks/useNotifications";
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  useNotifications();
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Feed"
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ListingEdit"
+        component={ListingEditScreen}
+        options={({ navigation }) => ({
+          tabBarButton: () => (
+            <NewListingButton
+              onPress={() => navigation.navigate("ListingEdit")}
+            />
+          ),
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="plus-circle"
+              size={size}
+              color={color}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default AppNavigator;
